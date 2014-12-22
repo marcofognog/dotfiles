108cc56f01520d48ad17ce5ae62fecc39aea3191
Marco Antonio <marcofognog@gmail.com>
2014-12-22 19:54:34 -0200

    Descriptive comments.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
+# Vim config
 cp .vimrc ~/.vimrc
 cp -r .vim/* ~/.vim
+
+# urxvt config
 cp .Xresources ~/
 xrdb .Xresources
+
+# I3 config
+cp -r .i3/* ~/.i3
 
 sudo apt-get install xdotool i3 curl xclip rxvt-unicode git
 sudo cp urxvtclip /usr/lib/urxvt/perl/clipboard
