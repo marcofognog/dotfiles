6f28fcdcf68a1e7d91370d0ee43eedcce1500c77
Marco Antonio <marcofognog@gmail.com>
2014-12-22 19:06:01 -0200

    Set the config for urxvt.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 cp .vimrc ~/.vimrc
 cp -r .vim/* ~/.vim
+cp .Xresources ~/
+xrdb .Xresources
 
 sudo apt-get install xdotool i3 curl xclip rxvt-unicode git
 sudo cp urxvtclip /usr/lib/urxvt/perl/clipboard
