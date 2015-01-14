9fc66313f27e94726fc2e6eb807b1b6b4db1332d
Marco Antonio <marcofognog@gmail.com>
2015-01-14 18:27:30 -0200

    better i3 installation.

--- install.sh
+++ install.sh
@@ -8,10 +8,17 @@ cp -r .vim/* ~/.vim
 cp .Xresources ~/
 xrdb .Xresources
 
+# Install recent I3wm
+echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get --allow-unauthenticated install sur5r-keyring
+sudo apt-get update
+sudo apt-get install i3
+
 # I3 config
 cp -r .i3/* ~/.i3
 
-sudo apt-get install xdotool i3 curl xclip rxvt-unicode git
+sudo apt-get install xdotool treE curl xclip rxvt-unicode
 sudo cp urxvtclip /usr/lib/urxvt/perl/clipboard
 
 # Finding files automated
