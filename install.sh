a4dc4e097888709e3dea301c88f498a6e4a00e1f
Marco Nogueira <marconogueira@idc6886.(none)>
2015-04-28 15:10:05 -0300

    Yes to all instalations

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ xrdb .Xresources
 # Install recent I3wm
 echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get --allow-unauthenticated install sur5r-keyring
+sudo apt-get -y --allow-unauthenticated install sur5r-keyring
 sudo apt-get update
-sudo apt-get install i3
+sudo apt-get -y install i3
 
 # I3 config
 cp -r .i3/* ~/.i3
 
-sudo apt-get install xdotool treE curl xclip rxvt-unicode
+sudo apt-get -y install xdotool treE curl xclip rxvt-unicode
 sudo cp urxvtclip /usr/lib/urxvt/perl/clipboard
 
 # Finding files automated
