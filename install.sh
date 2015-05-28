a24d4e7280fdb4f31b58c10098b48fe3747e31d1
Marcao <marcofognog@gmail.com>
2015-05-28 13:42:15 -0300

    Verbose and stop exec when fails.

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
+set -xe
+
 cp .bashrc ~/.bashrc
 cp -r .bashrc.d ~/.bashrc.d
 cp .gitconfig ~/.gitconfig
 
 #---- I3 ----
-echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -y --allow-unauthenticated install sur5r-keyring
 sudo apt-get update
 sudo apt-get -y install i3
 
 echo "---------------------------- >creating i3 dir"
-mkdir ~/.i3
-cp -r .i3/* ~/.i3
+cp -r .i3/ ~/.i3
 
 # ---- VIM ----
 ./build-vim.sh
