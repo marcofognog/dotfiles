357e45512885ab54368276a891931ecce330266d
Marcao <marcofognog@gmail.com>
2015-07-06 18:10:58 -0300

    Groups all dotifile cps

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 set -xe
 
-cp .bashrc ~/.bashrc
-cp -r .bashrc.d ~/.bashrc.d
-cp .gitconfig ~/.gitconfig
 
 #---- I3 ----
 sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
@@ -11,15 +8,8 @@ sudo apt-get -y --allow-unauthenticated install sur5r-keyring
 sudo apt-get update
 sudo apt-get -y install i3
 
-echo "---------------------------- >creating i3 dir"
-cp -r .i3/ ~/.i3
-
 # ---- VIM ----
 ./build-vim.sh
-
-# Vim config
-cp .vimrc ~/.vimrc
-cp -r .vim/* ~/.vim
 
 git clone https://github.com/gmarik/vundle.git /tmp/vundle
 
@@ -50,3 +40,12 @@ fi
 mv /tmp/.fzf ~/.fzf
 
 ~/.fzf/install
+
+cp .bashrc ~/.bashrc
+cp -r .bashrc.d ~/.bashrc.d
+cp .gitconfig ~/.gitconfig
+cp -r .i3/ ~/.i3
+
+# Vim config
+cp .vimrc ~/.vimrc
+cp -r .vim/* ~/.vim
