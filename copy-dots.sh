ad757f262a1238b844397520a0e0bbe7eed85246
Marcao <marcofognog@gmail.com>
2015-07-06 18:37:10 -0300

    Another file for copy files.

---
+++ copy-dots.sh
@@ -0,0 +1,8 @@
+cp .bashrc ~/.bashrc
+cp -r .bashrc.d ~/.bashrc.d
+cp .gitconfig ~/.gitconfig
+cp -r .i3/ ~/.i3
+
+# Vim config
+cp .vimrc ~/.vimrc
+cp -r .vim/* ~/.vim
--- install.sh
+++ install.sh
@@ -41,11 +41,4 @@ mv /tmp/.fzf ~/.fzf
 
 ~/.fzf/install
 
-cp .bashrc ~/.bashrc
-cp -r .bashrc.d ~/.bashrc.d
-cp .gitconfig ~/.gitconfig
-cp -r .i3/ ~/.i3
-
-# Vim config
-cp .vimrc ~/.vimrc
-cp -r .vim/* ~/.vim
+./copy-dots.sh
