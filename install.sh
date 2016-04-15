85faf5a26988e02aeba1af8663c3ff4d69a1687a
Marcao <marcofognog@gmail.com>
2016-04-15 20:30:52 -0300

    ADd google-chrome.

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@ set -xe
 
 sudo add-apt-repository ppa:shutter/ppa
 
-#---- I3 ----
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
 sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -y --allow-unauthenticated install sur5r-keyring
 sudo apt-get update
-sudo apt-get -y install i3 shutter
+sudo apt-get -y install i3 shutter google-chrome-stable
 
 # ---- VIM ----
 ./build-vim.sh
