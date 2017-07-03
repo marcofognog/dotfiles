61abf6906e3db80ab8bbb2c7755c3b8a4ff0e96c
Marcao <marcofognog@gmail.com>
2017-07-03 12:24:49 -0300

    Add production support.

--- ticket_select.sh
+++ ticket_select.sh
@@ -1,17 +1,23 @@
 tkt() {
-  echo "$(curl -iks -XPOST -d "username=$1&password=$2" $3/v1/tickets | grep "Location: " | sed 's/Location\: //' | tr -d '\r')"
+    echo "$(curl -iks -XPOST -d "username=$1&password=$2" $3/v1/tickets | grep "Location: " | sed 's/Location\: //' | tr -d '\r')"
 }
 
 st() {
-  local ticket=$(tkt $2 $3 $4)
-  echo $(curl -ksb -XPOST -d "service=$1" "$ticket")
+    local ticket=$(tkt $2 $3 $4)
+    echo $(curl -ksb -XPOST -d "service=$1" "$ticket")
 }
 
 target=$(cat $HOME/.cas-ticket-targets | fzf --select-1 --exit-0)
 cas_service=$(echo $target | cut -d',' -f1)
 cas_user=$(echo $target | cut -d',' -f2)
 cas_pass=$(echo $target | cut -d',' -f3)
-cas_url=https://systems-login.systemintegration.locaweb.com.br
+env_code=$(echo $target | cut -d',' -f4)
+if [ $env_code == "prod" ]
+then
+   cas_url=https://systems-login.locaweb.com.br
+else
+   cas_url=https://systems-login.systemintegration.locaweb.com.br
+fi
 
 result=$(st $cas_service $cas_user $cas_pass $cas_url)
 echo $cas_service " => "  $result
