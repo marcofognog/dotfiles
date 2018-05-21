5b749150f29d85c630187115ef60ec9f688abdb7
Marcao <marcofognog@gmail.com>
2018-05-21 19:48:02 -0300

    not used anymore

--- install.sh
+++ install.sh
@@ -10,9 +10,6 @@ sudo apt-get -y --allow-unauthenticated install sur5r-keyring
 sudo apt-get update
 sudo apt-get -y install i3 shutter google-chrome-stable
 
-# Finding files automated
-sudo cp file-edit.sh /usr/local/bin
-
 # FZF finder
 git clone --depth 1 https://github.com/junegunn/fzf.git /tmp/.fzf
 
