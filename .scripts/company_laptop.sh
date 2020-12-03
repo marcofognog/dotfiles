7607948f1455902d4ff4bd2a6dc0e61f79ce493e
Marcao <marcofognog@gmail.com>
2020-12-03 10:08:49 +0100

    update monitor

--- .scripts/company_laptop.sh
+++ .scripts/company_laptop.sh
@@ -1,1 +1,1 @@
-xrandr --output eDP-1 --auto --output DP-2-3 --off
+xrandr --output eDP-1 --auto --output DP-1 --off
--- .scripts/company_monitor.sh
+++ .scripts/company_monitor.sh
@@ -1,1 +1,1 @@
-xrandr --output eDP-1 --off --output DP-2-3 --auto
+xrandr --output eDP-1 --off --output DP-1 --auto --mode 2560x1440
