b536cada29c1b9738cd57038d256b5b98e6d0459
Marco Antonio <marcofognog@gmail.com>
2022-03-19 10:36:26 +0100

    Improve set screen script

--- .scripts/set-screen.sh
+++ .scripts/set-screen.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-intern=LVDS-1
-extern=HDMI-1
+if  [ -z $SETUP_ENV ]; then
+    filepath=$(readlink -f "$0")
+    echo "SETUP_ENV is not defined (exited from $filepath)"
+    exit;
+fi
+
+if [ $SETUP_ENV = "asus" ]; then
+    intern=LVDS-1
+    extern=HDMI-1
+else
+    intern=eDP-1
+    extern=HDMI-2
+fi
 
 if xrandr | grep "$extern disconnected"; then
     xrandr --output $extern --off --output $intern --auto
